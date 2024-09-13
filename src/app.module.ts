04af0b54f893f404a33ff3b95168d9c893bb0d25
Fernando Muñoz <fernandofailed@gmail.com>
2024-09-13 01:37:29 -0300

    Creación de API "Palabra"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PalabrasModule } from './palabras/palabras.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PalabrasModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
