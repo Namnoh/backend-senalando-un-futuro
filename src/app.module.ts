1ef31cbe233d9ec97758c8e885d071cf723b7a1c
je4n83 <jean.vtedy@gmail.com>
2024-10-18 03:03:38 -0300

    se agrega niveles y progreso al appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { ConfigModule } from '@nestjs/config';  // Importar ConfigModule
 import { PalabrasModule } from './palabras/palabras.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
-
+import { NivelModule } from './niveles/niveles.module';
+import { ProgresoModule } from './progresos/progresos.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,  // Hace que las variables de entorno estén disponibles globalmente en toda la aplicación
     }),
     PalabrasModule, 
-    UsuariosModule
+    UsuariosModule,
+    NivelModule,
+    ProgresoModule
   ],
   controllers: [],
   providers: [PrismaService],
