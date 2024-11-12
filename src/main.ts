019db62a705a05673b6a2410ad38d56d6ba5eeb3
je4n83 <jean.vtedy@gmail.com>
2024-11-11 23:56:34 -0300

    se borran comentarios en el main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module'; // Asegúrate de que esto apunte correctamente
-
+import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   //app.useGlobalPipes(new ValidationPipe());
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,               // Convierte los datos al tipo que se espera en el DTO
-      whitelist: true,               // Elimina las propiedades no permitidas en el DTO
-      forbidNonWhitelisted: true,    // Lanza un error si hay propiedades no permitidas
-      disableErrorMessages: false,   // Habilita los mensajes de error detallados
+      transform: true,               
+      whitelist: true,               
+      forbidNonWhitelisted: true,    
+      disableErrorMessages: false,   
     })
   );
   app.enableCors();
